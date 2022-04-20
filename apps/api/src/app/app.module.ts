8da816b4dfb9f5a6029ebe550f907642f8ae46f9
sunilb <pavanksunil@gmail.com>
2022-04-20 23:30:56 +0530

    config changes

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static'; // <- INSERT LINE
+import { join } from 'path'; // <- INSERT LINE
 
 @Module({
-  imports: [],
+  imports: [// BEGIN INSERT
+  ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', 'nx-app'),
+    exclude: ['/api*']
+  })
+  // END INSERT
+],
   controllers: [AppController],
   providers: [AppService],
 })
