d17e2f2ea89eeb043df2837b3c2a04897c79b791
sunilb <pavanksunil@gmail.com>
2022-04-25 18:31:43 +0530

    list fixed

--- apps/carsforrent/src/app/results/results.component.ts
+++ apps/carsforrent/src/app/results/results.component.ts
@@ -1,15 +1,24 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CarRequest } from '../interfaces/interfaces';
+import { StoreCarService } from '../store-car.service';
 
 @Component({
   selector: 'carsforrent-results',
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.css'],
 })
-export class ResultsComponent {
-  constructor(private router: Router) {}
+export class ResultsComponent implements OnInit {
+  public cars: CarRequest[];
+  constructor(private router: Router, private carsService: StoreCarService) {}
 
+  ngOnInit() {
+    this.carsService.getAllCars().subscribe((res) => {
+      console.log(res);
+      this.cars = res;
+    });
+  }
   handleBookNow() {
     this.router.navigateByUrl('/review');
   }
