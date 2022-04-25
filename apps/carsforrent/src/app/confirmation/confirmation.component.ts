d4a4c360ef5f255e0463360bbd8bf68b575d5373
sunilb <pavanksunil@gmail.com>
2022-04-25 22:46:14 +0530

    confirmation page added

--- apps/carsforrent/src/app/confirmation/confirmation.component.ts
+++ apps/carsforrent/src/app/confirmation/confirmation.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'carsforrent-confirmation',
   templateUrl: './confirmation.component.html',
-  styleUrls: ['./confirmation.component.css']
+  styleUrls: ['./confirmation.component.css'],
 })
 export class ConfirmationComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  handleClick() {
+    this.router.navigate(['/']);
+    localStorage.removeItem('location');
+    localStorage.removeItem('dates');
+    localStorage.removeItem('car');
   }
-
+  ngOnInit(): void {}
 }
