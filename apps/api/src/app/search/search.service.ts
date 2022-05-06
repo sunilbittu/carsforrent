04d5f799ac8ae64df03f65e589208cb642c21ab8
sunilb <pavanksunil@gmail.com>
2022-05-07 00:18:13 +0530

    changes

--- apps/api/src/app/search/search.service.ts
+++ apps/api/src/app/search/search.service.ts
@@ -7,8 +7,10 @@ import { Bookings } from '../types/bookings';
 import { BookingDetailsDTO } from '../booking/booking.dto';
 import { Cars } from '../types/cars';
 
+
 @Injectable()
 export class SearchService {
+
   constructor(@InjectModel('BookingDetails') private bookingsModel: Model<Bookings>,
   @InjectModel('CarDetails') private carsModel: Model<Cars>) {}
   async getAvailability (details: SearchDetailsDTO): Promise<any[]> {
@@ -18,13 +20,33 @@ export class SearchService {
     const bookings = await this.bookingsModel.find();
     const carsList = await this.carsModel.find();
     const dates = [];
+    const cars = [];
     while (moment(formattedStartDate) <= moment(formattedEndDate)) {
       dates.push(moment(formattedStartDate).format('YYYY-MM-DD'));
       formattedStartDate = moment(formattedStartDate).add(1, 'days').toDate();
     }
-    const filterBooking = bookings.filter(booking => dates.includes(moment(booking.start).format('YYYY-MM-DD')) || dates.includes(moment(booking.end).format('YYYY-MM-DD')));
-    const filterCar = carsList.filter(car => filterBooking.every(booking => booking.id !== car.id));
-
+    console.log(dates);
+    for (let i = 0; i < bookings.length; i++) {
+     /*  console.log('sasasasasasasasa',moment(bookings[i].start).format('YYYY-MM-DD'),moment(bookings[i].end).format('YYYY-MM-DD'));
+      if(!dates.includes(moment(bookings[i].start).format('YYYY-MM-DD'))  || 
+      !dates.includes(moment(bookings[i].end).format('YYYY-MM-DD'))){
+        cars.push(bookings[i].id);
+      } */
+      let startDate = bookings[i].start;
+      const selectedDates = [];
+      while (moment(startDate) <= moment(bookings[i].end)) {
+        selectedDates.push(moment(startDate).format('YYYY-MM-DD'));
+        startDate = moment(startDate).add(1, 'days').toDate();
+      }
+      const isAvailable = dates.some(date => selectedDates.includes(date));
+      console.log(isAvailable);
+      if(!isAvailable){
+        cars.push(bookings[i].id);
+      }
+    }
+    // const filterBooking = bookings.filter(booking => dates.includes(moment(booking.start).format('YYYY-MM-DD')) && dates.includes(moment(booking.end).format('YYYY-MM-DD')));
+    const filterCar = carsList.filter(car => cars.includes(car.id));
+    console.log(filterCar);
     return filterCar
   }
 }
--- dist/apps/api/main.js
+++ dist/apps/api/main.js
@@ -1361,12 +1361,33 @@ let SearchService = class SearchService {
             const bookings = yield this.bookingsModel.find();
             const carsList = yield this.carsModel.find();
             const dates = [];
+            const cars = [];
             while (moment(formattedStartDate) <= moment(formattedEndDate)) {
                 dates.push(moment(formattedStartDate).format('YYYY-MM-DD'));
                 formattedStartDate = moment(formattedStartDate).add(1, 'days').toDate();
             }
-            const filterBooking = bookings.filter(booking => dates.includes(moment(booking.start).format('YYYY-MM-DD')) || dates.includes(moment(booking.end).format('YYYY-MM-DD')));
-            const filterCar = carsList.filter(car => filterBooking.every(booking => booking.id !== car.id));
+            console.log(dates);
+            for (let i = 0; i < bookings.length; i++) {
+                /*  console.log('sasasasasasasasa',moment(bookings[i].start).format('YYYY-MM-DD'),moment(bookings[i].end).format('YYYY-MM-DD'));
+                 if(!dates.includes(moment(bookings[i].start).format('YYYY-MM-DD'))  ||
+                 !dates.includes(moment(bookings[i].end).format('YYYY-MM-DD'))){
+                   cars.push(bookings[i].id);
+                 } */
+                let startDate = bookings[i].start;
+                const selectedDates = [];
+                while (moment(startDate) <= moment(bookings[i].end)) {
+                    selectedDates.push(moment(startDate).format('YYYY-MM-DD'));
+                    startDate = moment(startDate).add(1, 'days').toDate();
+                }
+                const isAvailable = dates.some(date => selectedDates.includes(date));
+                console.log(isAvailable);
+                if (!isAvailable) {
+                    cars.push(bookings[i].id);
+                }
+            }
+            // const filterBooking = bookings.filter(booking => dates.includes(moment(booking.start).format('YYYY-MM-DD')) && dates.includes(moment(booking.end).format('YYYY-MM-DD')));
+            const filterCar = carsList.filter(car => cars.includes(car.id));
+            console.log(filterCar);
             return filterCar;
         });
     }
